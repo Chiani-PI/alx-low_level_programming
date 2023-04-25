784580e1b03a985f4eff58d0f9adbed05f4af280
Chiani-PI <chianigwe@gmail.com>
2023-04-25 16:37:58 -0700

    First set 4

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _putchar - A program that prints _putchar, followed by a new line.
+ * main - A program that prints _putchar, followed by a new line.
  * Return: Always 0 (Success)
  */
 
-int _main(void)
+int main(void)
 {
 _putchar('_');
 _putchar('p');
