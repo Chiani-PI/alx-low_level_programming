0240943e95daade781eae867dfbd5e3800e13aba
Chiani-PI <chianigwe@gmail.com>
2023-04-26 06:02:47 -0700

    TS 15

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,39 +1,22 @@
 #include <stdio.h>
 /**
- * main -  prints the first 98 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-unsigned long int i;
-unsigned long int pre = 1;
-unsigned long int post = 2;
-unsigned long int pre1 = 0;
-unsigned long int pre2 = 0;
-unsigned long int post1 = 0;
-unsigned long int post2 = 0;
-unsigned long int l = 1000000000;
-printf("%lu", pre);
-for (i = 1; i <= 91; i++)
+unsigned int i, count;
+unsigned long int a = 1, b = 2, c;
+count = 2;
+printf("%lu, %lu", a, b);
+for (i = 3; i <= 98; i++)
 {
-printf(", %lu", post);
-post += pre;
-pre = post - pre;
-}
-pre1 = (pre / l);
-pre2 = (pre % l);
-post1 = (post / l);
-post2 = (post % l);
-for (i = 92; i <= 98; i++)
-{
-unsigned long int tmp = post2;
-printf(", %lu", post1 + (post2 / l));
-printf("%09lu", post2 % l);
-post2 = (pre2 + post2) % l;
-pre2 = tmp / l;
-post1 = post1 + pre1 + (post2 / l);
-pre1 = post1 - pre1 - (tmp % l < pre2 ? 1 : 0);
+c = a + b;
+printf(", %lu", c);
+a = b;
+b = c;
+count++;
 }
 printf("\n");
 return (0);
