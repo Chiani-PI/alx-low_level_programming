6ae7b19404b8611ca2baa91aa74e664b71930e83
Chiani-PI <chianigwe@gmail.com>
2023-05-04 00:04:33 -0700

    Corrections 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,6 +31,7 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar((prod / 10) + '0');
 					_putchar((prod % 10) + '0');
 				} else if (prod >= 100)
